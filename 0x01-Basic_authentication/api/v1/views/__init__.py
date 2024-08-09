b556d1aa57081bcf9749abda60df0a751712625c
Desheye <aarealaafiaboyz@gmail.com>
2024-08-09 10:26:10 +0000

    refixed

--- 0x01-Basic_authentication/api/v1/views/__init__.py
+++ 0x01-Basic_authentication/api/v1/views/__init__.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 """ DocDocDocDocDocDoc
 """
-from api.v1.views.users import *
-from api.v1.views.index import *
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
+# Import views after creating app_views
+from api.v1.views.index import *
+from api.v1.views.users import *
 
+# If you need to load User data, do it here
+from models.user import User
 User.load_from_file()
