b85f42b0053ee664397857be0908264c659140c7
Desheye <aarealaafiaboyz@gmail.com>
2024-08-09 11:06:10 +0000

    flask auth

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -4,7 +4,6 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
-
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -13,6 +12,21 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """GET /api/v1/unauthorized
+    Return:
+      - Aborts with a 401 error
+    """
+    abort(401)
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """GET /api/v1/forbidden
+    Return:
+      - Aborts with a 403 error
+    """
+    abort(403)
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
