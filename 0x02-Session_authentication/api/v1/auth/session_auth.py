ffa4c44eba89906aab1fe500b653f65c2a835850
Desheye <aarealaafiaboyz@gmail.com>
2024-08-09 13:21:21 +0000

    session authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,6 @@
-
 #!/usr/bin/env python3
-""" Module of Session Authentication
-"""
+""" Module of Session Authentication """
+
 from api.v1.auth.auth import Auth
 from models.user import User
 import uuid
@@ -64,4 +63,3 @@ class SessionAuth(Auth):
             pass
 
         return True
-
--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -8,7 +8,7 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 # Import views after creating app_views
 from api.v1.views.index import *  # noqa: E402
 from api.v1.views.users import *  # noqa: E402
-from api.v1.views.session_auth import * # noqa: E402
+from api.v1.views.session_auth import *  # noqa: E402
 
 # If you need to load User data, do it here
 from models.user import User  # noqa: E402
